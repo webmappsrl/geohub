bc8b93cac935edf46aded03c6dec582202f7425e
marco <marco@eniacom.com>
2021-05-03 17:24:12 +0200

    update UgcPoi test

--- app/Models/UgcPoi.php
+++ app/Models/UgcPoi.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Traits\GeometryFeatureTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Log;
 
 class UgcPoi extends Model
 {
@@ -19,15 +18,5 @@ class UgcPoi extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function save(array $options = [])
-    {
-        parent::save($options);
-        try {
-            $this->hoquServiceProvider->store('update_ugc_taxonomy_where', ['id' => $this->id]);
-        } catch (\Exception $e) {
-            Log::error('An error occurred during a store operation: ' . $e->getMessage());
-        }
     }
 }
