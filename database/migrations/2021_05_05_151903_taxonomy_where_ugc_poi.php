183d227d80b7dff61ab885d6d26b3b1e9bc0fa20
Davide Pizzato <davidepizzato@webmapp.it>
2021-05-06 16:51:54 +0200

    Fixed some migrations, raised api request limit

---
+++ database/migrations/2021_05_05_151903_taxonomy_where_ugc_poi.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TaxonomyWhereUgcPoi extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('taxonomy_where_ugc_poi', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ugc_poi_id')->unsigned();
+            $table->integer('taxonomy_where_id')->unsigned();
+            $table->foreign('ugc_poi_id')
+                ->references('id')
+                ->on('ugc_pois');
+            $table->foreign('taxonomy_where_id')
+                ->references('id')
+                ->on('taxonomy_wheres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('taxonomy_where_ugc_poi');
+    }
+}
---
+++ database/migrations/2021_05_05_152106_taxonomy_where_ugc_track.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TaxonomyWhereUgcTrack extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('taxonomy_where_ugc_track', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ugc_track_id')->unsigned();
+            $table->integer('taxonomy_where_id')->unsigned();
+            $table->foreign('ugc_track_id')
+                ->references('id')
+                ->on('ugc_tracks');
+            $table->foreign('taxonomy_where_id')
+                ->references('id')
+                ->on('taxonomy_wheres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('taxonomy_where_ugc_track');
+    }
+}
