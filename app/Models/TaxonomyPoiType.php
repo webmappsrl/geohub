337ab7d6d04fcc82b375b22c1fdf0599b87001eb
marco <marco@eniacom.com>
2021-05-11 17:41:32 +0200

    added model, factory and migration TaxonomyPoiType

---
+++ app/Models/TaxonomyPoiType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaxonomyPoiType extends Model
+{
+    use HasFactory;
+
+    public function author()
+    {
+        return $this->belongsTo("\App\Models\User", "user_id", "id");
+    }
+
+    public function save(array $options = [])
+    {
+        static::creating(function ($taxonomyPoiType) {
+            $taxonomyPoiType->author()->associate(User::getEmulatedUser());
+        });
+        parent::save($options);
+    }
+}
+
---
+++ database/factories/TaxonomyPoiTypeFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TaxonomyPoiType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class TaxonomyPoiTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TaxonomyPoiType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'excerpt' => $this->faker->text(90),
+            'description' => $this->faker->text(),
+            'source_id' => $this->faker->randomDigit,
+            'source' => $this->faker->text(100),
+            'admin_level' => $this->faker->randomDigit,
+            'import_method' => $this->faker->name(),
+        ];
+    }
+}
