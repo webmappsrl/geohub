d621a6610f3e81899612ae75b2f9a2021c7c4808
marco <marco@eniacom.com>
2021-05-12 12:19:14 +0200

    added model, factory and migration for taxonomy_themes

---
+++ database/factories/TaxonomyThemeFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TaxonomyTheme;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaxonomyThemeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TaxonomyTheme::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'excerpt' => $this->faker->text(90),
+            'description' => $this->faker->text(),
+            'source_id' => $this->faker->randomDigit(),
+            'source' => $this->faker->text(100),
+            'admin_level' => $this->faker->randomDigit(),
+            'import_method' => $this->faker->name(),
+        ];
+    }
+}
