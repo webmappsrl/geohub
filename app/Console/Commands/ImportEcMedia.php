0de7f54440f431122ac391d00bcd1a3a502b9563
marco <marco@eniacom.com>
2021-05-14 18:45:56 +0200

    updated command to upload image from url/path

--- app/Console/Commands/ImportEcMedia.php
+++ app/Console/Commands/ImportEcMedia.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class ImportEcMedia extends Command
 {
@@ -11,7 +12,7 @@ class ImportEcMedia extends Command
      *
      * @var string
      */
-    protected $signature = 'geoghub:import_ec_media 
+    protected $signature = 'geoghub:import_ec_media
                             {url : Url or path of the image to store in the server}';
 
     /**
@@ -38,7 +39,16 @@ class ImportEcMedia extends Command
      */
     public function handle()
     {
-        echo $this->argument('url');
+        $url = $this->argument('url');
+        $path = "/public/EcMedia/";
+        $file = @file_get_contents($url);
+        if ($file === FALSE)
+            return $this->error('Error, file does not exists');
+        $contents = file_get_contents($url);
+        $name = substr($url, strrpos($url, '/') + 1);
+        Storage::put($path . substr(str_shuffle(MD5(microtime())), 0, 5), $contents);
+
+        $this->info("File uploaded correctly");
         return 0;
     }
 }
