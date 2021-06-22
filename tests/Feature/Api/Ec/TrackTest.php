d4a851bc4c46bd40e639ab7e77c82152da904c9b
Roberto Dessì <dessi@netseven.it>
2021-06-22 12:48:01 +0200

    0.1.8.16

--- tests/Feature/Api/Ec/TrackTest.php
+++ tests/Feature/Api/Ec/TrackTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Api\Ec;
 use App\Models\EcTrack;
 use App\Models\TaxonomyWhere;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class TrackTest extends TestCase
@@ -72,8 +73,42 @@ class TrackTest extends TestCase
 
     public function testDownloadGpxData()
     {
-        $ecTrack = EcTrack::factory()->create();
+        $data['name'] = 'Test track';
+        $data['description'] = 'Test track description.';
+        $data['geometry'] = DB::raw("(ST_GeomFromText('LINESTRING(11 43,12 43,12 44,11 44)'))");
+        $gpx = <<<KML
+<?xml version="1.0"?>
+<gpx version="1.1" creator="GDAL 2.2.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ogr="http://osgeo.org/gdal" xmlns="http://www.topografix.com/GPX/1/1" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
+<trk><trkseg><trkpt lon="11" lat="43"></trkpt><trkpt lon="12" lat="43"></trkpt><trkpt lon="12" lat="44"></trkpt><trkpt lon="11" lat="44"></trkpt></trkseg></trk>
+</gpx>
+KML;
+
+        $ecTrack = EcTrack::factory()->create($data);
         $result = $this->getJson('/api/ec/track/download/' . $ecTrack->id . '/gpx', []);
         $this->assertEquals(200, $result->getStatusCode());
+        
+        $gpxResponse = $result->getContent();
+
+        $this->assertEquals($gpxResponse, $gpx);
+    }
+
+    public function testDownloadKmlData()
+    {
+        $data['name'] = 'Test track';
+        $data['description'] = 'Test track description.';
+        $data['geometry'] = DB::raw("(ST_GeomFromText('LINESTRING(11 43,12 43,12 44,11 44)'))");
+        $kml = <<<KML
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+<Placemark><name>Test track</name><description>Test track description.</description><LineString><coordinates>11,43 12,43 12,44 11,44</coordinates></LineString></Placemark>
+</kml>
+KML;
+        $ecTrack = EcTrack::factory()->create($data);
+        $result = $this->getJson('/api/ec/track/download/' . $ecTrack->id . '/kml', []);
+        $this->assertEquals(200, $result->getStatusCode());
+        
+        $kmlResponse = $result->getContent();
+
+        $this->assertEquals($kmlResponse, $kml);
     }
 }
