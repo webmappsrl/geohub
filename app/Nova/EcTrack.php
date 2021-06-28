c7a4664c9f283b791ad11831ce5af8780599b1a7
Alessio Piccioli <piccioli@netseven.it>
2021-06-28 18:44:31 +0200

    Bug fix removed osmid

--- app/Nova/EcTrack.php
+++ app/Nova/EcTrack.php
@@ -77,7 +77,8 @@ class EcTrack extends Resource
 
             new Panel('Taxonomies', $this->attach_taxonomy()),
             Text::make(__('Name'), 'name')->sortable(),
-            Text::make(__('Osm ID'), 'osm_id'),
+            Text::make(__('Import Method'), 'import_method'),
+            Text::make(__('Source ID'), 'source_id'),
             BelongsTo::make('Author', 'author', User::class)->sortable()->hideWhenCreating()->hideWhenUpdating(),
             BelongsToMany::make('EcMedia'),
             CKEditor::make(__('Description'), 'description')->hideFromIndex(),
