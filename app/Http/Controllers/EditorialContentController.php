7ab3a09109cbdef9b085a5661d7e825db95e7f0b
Roberto Dessì <dessi@netseven.it>
2021-06-30 17:30:21 +0200

    0.1.12.12 app elbrus API gallery

--- app/Http/Controllers/EditorialContentController.php
+++ app/Http/Controllers/EditorialContentController.php
@@ -209,6 +209,18 @@ class EditorialContentController extends Controller
             $geojson['properties']['image'] = json_decode($track->featureImage->getJson(), true);
         }
 
+        if ($track->ecMedia) {
+            $gallery = [];
+            $ecMedia = $track->ecMedia;
+            foreach ($ecMedia as $media) {
+                $gallery[] = json_decode($media->getJson(), true);
+            }
+
+            if (count($gallery)) {
+                $geojson['properties']['imageGallery'] = $gallery;
+            }
+        }
+
         // Add Taxonomies
         $taxonomies = $this->_getTaxonomies($track, $names = ['activity', 'theme', 'where', 'who', 'when']);
         $geojson['properties']['taxonomy'] = $taxonomies;
