9bfdffced4bad00285952bcf1beb744d0add2bff
Davide Pizzato <davidepizzato@webmapp.it>
2021-07-27 17:52:57 +0200

    Changed ugc media name to nullable

--- database/migrations/2021_07_22_104434_add_media_fields_to_ugc_media.php
+++ database/migrations/2021_07_22_104434_add_media_fields_to_ugc_media.php
@@ -4,17 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddMediaFieldsToUgcMedia extends Migration
-{
+class AddMediaFieldsToUgcMedia extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::table('ugc_media', function (Blueprint $table) {
-            $table->text('name');
+            $table->text('name')->nullable();
             $table->text('description')->nullable();
             $table->json('raw_data')->nullable();
         });
@@ -25,8 +23,7 @@ class AddMediaFieldsToUgcMedia extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::table('ugc_media', function (Blueprint $table) {
             $table->dropColumn('name');
             $table->dropColumn('description');
