ab4072a7e8316671b56841eb6724d1cc0cd2c33a
Pedram Katanchi <pedramkat@gmail.com>
2021-09-16 18:06:46 +0200

    Added more tests to TrackSharePageTest

--- resources/views/components/trackContentSection.blade.php
+++ resources/views/components/trackContentSection.blade.php
@@ -1,6 +1,6 @@
 @props(['track'])
 
-<div class="px-4 py-4">
+<div id="trackDescription" class="px-4 py-4">
     @if ($track->description)
         <h3 class="text-primary font-semibold text-xl">Descrizione</h3>
         <p>{{$track->description}}</p>
--- tests/Feature/Web/TrackSharePageTest.php
+++ tests/Feature/Web/TrackSharePageTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Models\EcTrack;
+use App\Models\EcMedia;
+use App\Models\TaxonomyWhere;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,17 +15,17 @@ class TrackSharePageTest extends TestCase
     /**
      * @test
      */
-    public function when_user_access_to_track_with_not_existing_id_then_it_returns_404()
-    {
+    public function when_visitor_access_to_track_with_not_existing_id_then_it_returns_404()
+    {   
+        EcTrack::truncate();
         $track = EcTrack::factory()->create();
-        while( in_array( ($n = mt_rand(1,100)), array($track->id) ) );
-        $response = $this->get('/track/'.$n);
+        $response = $this->get('/track/'.(intval($track->id) + 1));
         $response->assertStatus(404);
     }
     /**
      * @test
      */
-    public function when_user_access_to_track_with_existing_id_then_it_returns_200()
+    public function when_visitor_access_to_track_with_existing_id_then_it_returns_200()
     {
         $track = EcTrack::factory()->create();
         $response = $this->get('/track/'.$track->id);
@@ -32,10 +34,156 @@ class TrackSharePageTest extends TestCase
     /**
      * @test
      */
-    public function when_user_access_to_track_check_the_title_is_correct()
+    public function when_visitor_access_to_track_then_the_title_is_correct()
     {
         $track = EcTrack::factory()->create();
         $response = $this->get('/track/'.$track->id);
         $response->assertSee($track->name);
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_with_feature_image_then_feature_image_is_correct()
+    {
+        $m = EcMedia::factory()->frontEndSizes()->create();
+        $t = EcTrack::factory()->create([
+            'feature_image' => $m->id
+        ]);
+        $r = $this->get('/track/'.$t->id);
+        $r->assertSee($m->thumbnail('1440x500'));
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_without_feature_image_then_it_returns_200()
+    {
+        $t = EcTrack::factory()->create([
+            'feature_image' => null
+        ]);
+        $r = $this->get('/track/'.$t->id);
+        $r->assertStatus(200);
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_without_feature_image_then_feature_image_is_placeholder()
+    {
+        $t = EcTrack::factory()->create([
+            'feature_image' => null
+        ]);
+        $r = $this->get('/track/'.$t->id);
+        $title = explode("/", config("geohub.ectrack_share_page_feature_image_placeholder"));
+        $r->assertSee(end($title));
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_with_taxonomy_wheres_then_taxonomies_are_correct()
+    {
+        $tax1 = TaxonomyWhere::factory()->create();
+        $tax2 = TaxonomyWhere::factory()->create();
+        $t = EcTrack::factory()->create();
+        $t->taxonomyWheres()->attach([$tax1->id,$tax2->id]);
+        $r = $this->get('/track/'.$t->id);
+        $r->assertSee($tax1->name);
+        $r->assertSee($tax2->name);
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_without_taxonomy_wheres_then_it_returns_200()
+    {
+        $t = EcTrack::factory()->create();
+        if ($t->taxonomyWheres()->count() > 0) {
+            foreach ($t->taxonomyWheres() as $tax) {
+                $t->detach($tax);
+            }
+        }
+        $r = $this->get('/track/'.$t->id);
+        $r->assertStatus(200);
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_without_taxonomy_wheres_then_div_class_taxonomyWheres_does_not_exists()
+    {
+        $t = EcTrack::factory()->create();
+        if ($t->taxonomyWheres()->count() > 0) {
+            foreach ($t->taxonomyWheres() as $tax) {
+                $t->detach($tax);
+            }
+        }
+        $r = $this->get('/track/'.$t->id);
+        $r->assertDontSee('taxonomyWheres');
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_with_taxonomy_activities_then_taxonomies_are_correct()
+    {
+
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_without_taxonomy_activities_then_it_returns_200()
+    {
+
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_without_taxonomy_activities_then_div_id_taxonomyActivities_does_not_exist()
+    {
+
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_with_description_then_description_is_correct()
+    {
+
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_without_description_then_it_returns_200()
+    {
+
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_without_description_then_div_id_trackDescription_is_empty()
+    {
+
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_with_track_details_then_details_are_correct()
+    {
+
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_with_gallery_then_gallery_images_are_correct()
+    {
+
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_without_gallery_then_it_returns_200()
+    {
+
+    }
+    /**
+     * @test
+     */
+    public function when_visitor_access_to_track_without_gallery_then_div_id_carousel_does_not_exist()
+    {
+
     }
 }
