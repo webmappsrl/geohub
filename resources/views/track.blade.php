116aeddb902c183db8d301b66f3595835fafb3c6
Pedram Katanchi <pedramkat@gmail.com>
2021-09-16 13:00:53 +0200

    Added all the components of Track page

--- resources/views/track.blade.php
+++ resources/views/track.blade.php
@@ -1,10 +1,16 @@
+@php
+    use Jenssegers\Agent\Agent;
+    $agent = new Agent();
+@endphp
+
 <x-trackLayout :track="$track">
-    <x-trackHeader :track="$track" />
-    <div class=""><p>{{$track->excerpt}}</p></div>
-    <div class="">
-        <h2>Descrizione:</h2>
-        <p>{{$track->description}}</p>
-    </div>
-    
-    
+    <x-trackHeader :track="$track" :agent="$agent"/>
+    <main class="max-w-screen-xl m-auto pb-20 sm:pb-0">
+        <x-trackMapSection :track="$track" />
+        <x-trackContentSection :track="$track" />
+        <x-carouselSection :track="$track" />
+        @if ($agent->isMobile())
+            <x-trackMobileDownloadSection :track="$track" :agent="$agent"/>
+        @endif
+    </main>
 </x-trackLayout>
