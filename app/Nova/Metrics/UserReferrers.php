6421ab4b78afa0c76e42c24b02741e01742c3408
Davide Pizzato <davidepizzato@webmapp.it>
2021-10-11 16:55:05 +0200

    Added new partition metric in homepage to show the different user referrers

---
+++ app/Nova/Metrics/UserReferrers.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Nova\Metrics;
+
+use App\Models\User;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Metrics\Partition;
+use Laravel\Nova\Metrics\PartitionResult;
+
+class UserReferrers extends Partition {
+    /**
+     * Calculate the value of the metric.
+     *
+     * @param NovaRequest $request
+     *
+     * @return PartitionResult
+     */
+    public function calculate(NovaRequest $request): PartitionResult {
+        return $this->count($request, User::class, 'referrer')
+            ->label(function ($value) {
+                if ($value == null)
+                    return 'None';
+                else {
+                    $str = $value;
+                    if (substr($value, 0, 10) == 'it.webmapp') {
+                        $array = explode('.', $value);
+
+                        $str = end($array);
+                    }
+
+                    return substr($str, 0, 10) . '...';
+                }
+            });
+    }
+
+    /**
+     * Determine for how many minutes the metric should be cached.
+     *
+     * @return  \DateTimeInterface|\DateInterval|float|int
+     */
+    public function cacheFor() {
+        // return now()->addMinutes(5);
+    }
+
+    /**
+     * Get the URI key for the metric.
+     *
+     * @return string
+     */
+    public function uriKey(): string {
+        return 'user-referrers';
+    }
+}
