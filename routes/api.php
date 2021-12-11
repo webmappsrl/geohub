506a430fd97d2d699a9325bfa51f6aa199391dd5
Davide Pizzato <pizzatodavide@hotmail.it>
2021-12-11 16:36:16 +0100

    Fixed api url

--- routes/api.php
+++ routes/api.php
@@ -133,7 +133,7 @@ Route::name('api.')->group(function () {
         Route::prefix('media')->name('media.')->group(function () {
             Route::get("/geojson/{id}", [UserGeneratedDataController::class, 'getUgcGeojson'])->name('geojson');
             Route::post("/taxonomy_where", [UserGeneratedDataController::class, 'associateTaxonomyWhereWithUgcFeature'])->name('associate');
-            Route::get("/{id}", [UgcMediaController::class, 'download'])->name('download');
+            Route::get("/download/{id}", [UgcMediaController::class, 'download'])->name('download');
         });
     });
 
