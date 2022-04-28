420cbe8f781c46597ba5783b3b2b99528a0ec718
Pedram Katanchi <pedramkat@gmail.com>
2022-04-28 14:37:29 +0200

    Added migration add_raw_data_to_out_source_features

---
+++ database/migrations/2022_04_28_123200_add_raw_data_to_out_source_features.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRawDataToOutSourceFeatures extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('out_source_features', function (Blueprint $table) {
+            $table->jsonb('raw_data')->nullable();
+            $table->string('endpoint')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('out_source_features', function (Blueprint $table) {
+            $table->dropColumn('raw_data');
+            $table->dropColumn('endpoint');
+        });
+    }
+}
