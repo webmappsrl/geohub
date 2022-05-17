9c4f624489b88921852191ac8368d1620bf7b1ab
Pedram Katanchi <pedramkat@gmail.com>
2022-05-17 15:30:11 +0200

    geohub.222.15.3 added language en to tab-translatable.php and added en and de stubs json

--- tests/Feature/OutSourceImporter/OutSourceImporterFeatureWPImportTrackTest.php
+++ tests/Feature/OutSourceImporter/OutSourceImporterFeatureWPImportTrackTest.php
@@ -20,10 +20,16 @@ class OutSourceImporterFeatureWPImportTrackTest extends TestCase
         $type = 'track';
         $endpoint = 'https://stelvio.wp.webmapp.it';
         $source_id = 6;
+        $source_id_en = 1239;
+        $source_id_de = 1241;
         $stelvio_track = file_get_contents(base_path('tests/Feature/Stubs/stelvio_track.json'));
+        $stelvio_track_en = file_get_contents(base_path('tests/Feature/Stubs/stelvio_track_en.json'));
+        $stelvio_track_de = file_get_contents(base_path('tests/Feature/Stubs/stelvio_track_de.json'));
         $url = $endpoint.'/wp-json/wp/v2/track/'.$source_id;
+        $url_en = $endpoint.'/wp-json/wp/v2/track/'.$source_id_en;
+        $url_de = $endpoint.'/wp-json/wp/v2/track/'.$source_id_de;
 
-        // PREPARE MOCK
+        // PREPARE MOCK ITA
         $this->mock(CurlServiceProvider::class,function (MockInterface $mock) use ($stelvio_track,$url){
             $mock->shouldReceive('exec')
             ->atLeast(1)
@@ -46,7 +52,7 @@ class OutSourceImporterFeatureWPImportTrackTest extends TestCase
         // TODO: add some checks on geometry
         // TODO: add some checks on raw_data
         // This is not working:
-        // $this->assertEquals($stelvio_track,$out_source->raw_data);
+        $this->assertEquals($stelvio_track,json_encode($out_source->raw_data));
 
 
     }
