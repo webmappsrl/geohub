ac1505a58b20ba56558c0a68fe8856cf8200da23
Pedram Katanchi <pedramkat@gmail.com>
2022-06-01 15:24:46 +0200

    Test fix \OutSourceImporterFeatureWPImportTrackTest

--- tests/Feature/OutSourceImporter/OutSourceImporterFeatureWPImportTrackTest.php
+++ tests/Feature/OutSourceImporter/OutSourceImporterFeatureWPImportTrackTest.php
@@ -23,30 +23,56 @@ class OutSourceImporterFeatureWPImportTrackTest extends TestCase
         $source_id = 6;
         $source_id_en = 1239;
         $source_id_de = 1241;
+        $media_id = 1529;
         $stelvio_track_it = file_get_contents(base_path('tests/Feature/Stubs/stelvio_track.json'));
         $stelvio_track_en = file_get_contents(base_path('tests/Feature/Stubs/stelvio_track_en.json'));
         $stelvio_track_de = file_get_contents(base_path('tests/Feature/Stubs/stelvio_track_de.json'));
+        $stelvio_media = file_get_contents(base_path('tests/Feature/Stubs/stelvio_media.json'));
         $url_it = $endpoint.'/wp-json/wp/v2/track/'.$source_id;
         $url_en = $endpoint.'/wp-json/wp/v2/track/'.$source_id_en;
         $url_de = $endpoint.'/wp-json/wp/v2/track/'.$source_id_de;
+        $url_media = $endpoint.'/wp-json/wp/v2/media/'.$media_id;
+        $url_media2 = $endpoint.'/wp-json/wp/v2/media/2482';
+        $url_media3 = $endpoint.'/wp-json/wp/v2/media/2483';
+        $url_media4 = $endpoint.'/wp-json/wp/v2/media/2475';
 
 
         // PREPARE MOCK ITA
-        $this->mock(CurlServiceProvider::class,function (MockInterface $mock) use ($stelvio_track_it,$url_it,$url_en,$stelvio_track_en,$url_de,$stelvio_track_de){
+        $this->mock(CurlServiceProvider::class,function (MockInterface $mock) use ($stelvio_track_it,$url_it,$url_en,$stelvio_track_en,$url_de,$stelvio_track_de,$stelvio_media,$url_media,$url_media2,$url_media3,$url_media4){
             $mock->shouldReceive('exec')
-            ->once()
+            ->atleast(1)
             ->with($url_it)
             ->andReturn($stelvio_track_it);
 
             $mock->shouldReceive('exec')
-            ->once()
+            ->atleast(1)
             ->with($url_en)
             ->andReturn($stelvio_track_en);
 
             $mock->shouldReceive('exec')
-            ->once()
+            ->atleast(1)
             ->with($url_de)
             ->andReturn($stelvio_track_de);
+            
+            $mock->shouldReceive('exec')
+            ->atleast(1)
+            ->with($url_media)
+            ->andReturn($stelvio_media);
+            
+            $mock->shouldReceive('exec')
+            ->atleast(1)
+            ->with($url_media2)
+            ->andReturn($stelvio_media);
+            
+            $mock->shouldReceive('exec')
+            ->atleast(1)
+            ->with($url_media3)
+            ->andReturn($stelvio_media);
+            
+            $mock->shouldReceive('exec')
+            ->atleast(1)
+            ->with($url_media4)
+            ->andReturn($stelvio_media);
         });
 
         // FIRE
@@ -92,12 +118,18 @@ class OutSourceImporterFeatureWPImportTrackTest extends TestCase
         $source_id = 6;
         $source_id_en = 1239;
         $source_id_de = 1241;
+        $media_id = 1529;
         $stelvio_track_it = file_get_contents(base_path('tests/Feature/Stubs/stelvio_track.json'));
         $stelvio_track_en = file_get_contents(base_path('tests/Feature/Stubs/stelvio_track_en.json'));
         $stelvio_track_de = file_get_contents(base_path('tests/Feature/Stubs/stelvio_track_de.json'));
+        $stelvio_media = file_get_contents(base_path('tests/Feature/Stubs/stelvio_media.json'));
         $url_it = $endpoint.'/wp-json/wp/v2/track/'.$source_id;
         $url_en = $endpoint.'/wp-json/wp/v2/track/'.$source_id_en;
         $url_de = $endpoint.'/wp-json/wp/v2/track/'.$source_id_de;
+        $url_media = $endpoint.'/wp-json/wp/v2/media/'.$media_id;
+        $url_media2 = $endpoint.'/wp-json/wp/v2/media/2482';
+        $url_media3 = $endpoint.'/wp-json/wp/v2/media/2483';
+        $url_media4 = $endpoint.'/wp-json/wp/v2/media/2475';
 
         $OSF_poi_1 = OutSourcePoi::factory()->create([
             'provider' => 'App\Classes\OutSourceImporter\OutSourceImporterFeatureWP',
@@ -125,21 +157,41 @@ class OutSourceImporterFeatureWPImportTrackTest extends TestCase
         ]);
 
         // PREPARE MOCK ITA
-        $this->mock(CurlServiceProvider::class,function (MockInterface $mock) use ($stelvio_track_it,$url_it,$url_en,$stelvio_track_en,$url_de,$stelvio_track_de){
+        $this->mock(CurlServiceProvider::class,function (MockInterface $mock) use ($stelvio_track_it,$url_it,$url_en,$stelvio_track_en,$url_de,$stelvio_track_de,$stelvio_media,$url_media,$url_media2,$url_media3,$url_media4){
             $mock->shouldReceive('exec')
-            ->once()
+            ->atleast(1)
             ->with($url_it)
             ->andReturn($stelvio_track_it);
 
             $mock->shouldReceive('exec')
-            ->once()
+            ->atleast(1)
             ->with($url_en)
             ->andReturn($stelvio_track_en);
 
             $mock->shouldReceive('exec')
-            ->once()
+            ->atleast(1)
             ->with($url_de)
             ->andReturn($stelvio_track_de);
+            
+            $mock->shouldReceive('exec')
+            ->atleast(1)
+            ->with($url_media)
+            ->andReturn($stelvio_media);
+            
+            $mock->shouldReceive('exec')
+            ->atleast(1)
+            ->with($url_media2)
+            ->andReturn($stelvio_media);
+            
+            $mock->shouldReceive('exec')
+            ->atleast(1)
+            ->with($url_media3)
+            ->andReturn($stelvio_media);
+            
+            $mock->shouldReceive('exec')
+            ->atleast(1)
+            ->with($url_media4)
+            ->andReturn($stelvio_media);
         });
 
         // FIRE
