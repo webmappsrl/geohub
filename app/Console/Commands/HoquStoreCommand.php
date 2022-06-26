b0b0fc717f2a1a08873ed0b10dcab7905808a8f1
Alessio Piccioli <piccioli@netseven.it>
2022-06-26 13:44:18 +0200

    Created new command hoqu_store

---
+++ app/Console/Commands/HoquStoreCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Providers\HoquServiceProvider;
+use Illuminate\Console\Command;
+
+class HoquStoreCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'geohub:hoqu_store
+                            {job : Name of the GEOMIXER job that must be executed. E.g. enrich_ec_track}
+                            {id : feature ID}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'It sends a single feature hoqu store command.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $job = $this->argument('job');
+        $id = $this->argument('id');
+        $this->info("Sending store to HOQU for job $job with ID $id");
+        $hoquServiceProvider = app(HoquServiceProvider::class);
+        $hoquServiceProvider->store($job, ['id' => $id]);
+    }
+}
