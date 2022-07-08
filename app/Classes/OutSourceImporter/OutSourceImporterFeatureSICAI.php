c9feaa68577203afcf7108eb7607e08930323f11
Pedram Katanchi <pedramkat@gmail.com>
2022-07-08 18:09:11 +0200

    Adding track import and sync for SICAI

--- app/Classes/OutSourceImporter/OutSourceImporterFeatureSICAI.php
+++ app/Classes/OutSourceImporter/OutSourceImporterFeatureSICAI.php
@@ -26,34 +26,22 @@ class OutSourceImporterFeatureSICAI extends OutSourceImporterFeatureAbstract {
     public function importTrack(){
         
         // Curl request to get the feature information from external source
-        $db = DB::connection('out_source_osm');
-        $track = $db->table('hiking_routes')
-            ->where('id',$this->source_id)
-            ->select([
-                'ref',
-                'name',
-                'cai_scale',
-                'from',
-                'to',
-                'geometry',
-                'duration_forward',
-                'description',
-                'note',
-                'website',
-                'distance',
-            ])
+        $db = DB::connection('out_source_sicai');
+        $track = $db->table('sentiero_italia.SI_Tappe')
+            ->where('id_2',$this->source_id)
             ->first();
 
         // prepare feature parameters to pass to updateOrCreate function
         Log::info('Preparing OSF Track with external ID: '.$this->source_id);
-        $this->params['geometry'] = DB::select("SELECT ST_AsText(ST_LineMerge('$track->geometry'))")[0]->st_astext;
+        $geometry = DB::select("SELECT ST_AsText(ST_LineMerge(ST_Transform('$track->geom',4326)))")[0]->st_astext;
+        $this->params['geometry'] = $geometry;
         $this->params['provider'] = get_class($this);
         $this->params['type'] = $this->type;
         $this->params['raw_data'] = json_encode($track);
 
         // prepare the value of tags data
         Log::info('Preparing OSF Track TAGS with external ID: '.$this->source_id);
-        $this->prepareTrackTagsJson($track);
+        $this->prepareTrackTagsJson($track,$geometry);
         $this->params['tags'] = $this->tags;
         Log::info('Finished preparing OSF Track with external ID: '.$this->source_id);
         Log::info('Starting creating OSF Track with external ID: '.$this->source_id);
@@ -130,24 +118,31 @@ class OutSourceImporterFeatureSICAI extends OutSourceImporterFeatureAbstract {
      * @param object $track The OutSourceFeature parameters to be added or updated 
      * 
      */
-    protected function prepareTrackTagsJson($track){
+    protected function prepareTrackTagsJson($track,$geometry){
         Log::info('Preparing OSF Track TRANSLATIONS with external ID: '.$this->source_id);
-        $this->tags['name']['it'] = $track->name;
-        $this->tags['description']['it'] = $track->description;
-        $this->tags['excerpt']['it'] = $track->note;
-        $this->tags['from'] = $track->from;
-        $this->tags['to'] = $track->to;
-        $this->tags['cai_scale'] = $track->cai_scale;
-        if (isset($track->website) && $track->website){
-            $related_url_name = parse_url($track->website);
-            $host = $track->website;
-            if (isset($related_url_name['host']) && $related_url_name['host']) {
-                $host = $related_url_name['host'];
-            }
-            $this->tags['related_url'][$host] = $track->website;
+        $this->tags['name']['it'] = $track->tappa;
+        if ($track->descrizione_sito) {
+            $this->tags['description']['it'] = $track->descrizione_sito;
         }
-        $this->tags['ref'] = $track->ref;
-
+        if ($track->partenza) {
+            $this->tags['from'] = $track->partenza;
+        }
+        if ($track->arrivo) {
+            $this->tags['to'] = $track->arrivo;
+        }
+        if ($track->difficolta && $track->difficolta !== 'Dato in aggiornamento') {
+            $this->tags['cai_scale'] = $track->difficolta;
+        }
+        // if ($track->percorribilità && $track->percorribilità == 'Non percorribile') {
+        //     $this->tags['not_accessible'] = true;
+        // }
+        // $p = DB::select("SELECT ST_AsText('$p[0]->geometry')");
+        // $pp = DB::select("SELECT id from out_source_features WHERE type='poi' and endpoint='sicai' and ST_Contains(ST_BUFFER('$geometry',500),geometry)");
+        // DB::select("SELECT id from out_source_features WHERE type='poi' and endpoint='sicai' and ST_Contains(ST_BUFFER(ST_GeomFromText('$geometry')::Geography),500),geometry)");
+        // DB::select("SELECT * as p from out_source_features where type='poi' and endpoint='sicai' and ST_contains(ST_BUFFER($geometry,500),p.geometry)"); 
+        // if () {
+        //     $this->tags['related_poi'][] = $osf_id;
+        // }
     }
     
     /**
@@ -192,36 +187,36 @@ class OutSourceImporterFeatureSICAI extends OutSourceImporterFeatureAbstract {
         // Processing the feature image of POI
         if (isset($poi['immagine']) && $poi['immagine']) {
             Log::info('Preparing OSF POI FEATURE_IMAGE with external POI ID: '.$this->source_id);
-            $base = 'https://sentieroitaliamappe.cai.it/index.php/view/media/getMedia?repository=sicaipubblico&project=SICAI_Pubblico&path=';
-            $this->tags['feature_image'] = $this->createOSFMedia($base,$poi['immagine'],$poi,000);
+            
+            $this->tags['feature_image'] = $this->createOSFMedia($poi['immagine'],$poi,000);
         }
         
         // Processing the gallery image of POI
         if (isset($poi['foto02']) && $poi['foto02']) {
             Log::info('Preparing OSF POI GALLERY foto02 with external POI ID: '.$this->source_id);
-            $base = 'https://sentieroitaliamappe.cai.it/index.php/view/media/getMedia?repository=sicaipubblico&project=SICAI_Pubblico&path=';
-            $this->tags['image_gallery'][] = $this->createOSFMedia($base,$poi['immagine'],$poi,001);
+            
+            $this->tags['image_gallery'][] = $this->createOSFMedia($poi['immagine'],$poi,001);
         }
         
         // Processing the gallery image of POI
         if (isset($poi['foto03']) && $poi['foto03']) {
             Log::info('Preparing OSF POI GALLERY foto03 with external POI ID: '.$this->source_id);
-            $base = 'https://sentieroitaliamappe.cai.it/index.php/view/media/getMedia?repository=sicaipubblico&project=SICAI_Pubblico&path=';
-            $this->tags['image_gallery'][] = $this->createOSFMedia($base,$poi['immagine'],$poi,003);
+            
+            $this->tags['image_gallery'][] = $this->createOSFMedia($poi['immagine'],$poi,003);
         }
         
         // Processing the gallery image of POI
         if (isset($poi['foto04']) && $poi['foto04']) {
             Log::info('Preparing OSF POI GALLERY foto04 with external POI ID: '.$this->source_id);
-            $base = 'https://sentieroitaliamappe.cai.it/index.php/view/media/getMedia?repository=sicaipubblico&project=SICAI_Pubblico&path=';
-            $this->tags['image_gallery'][] = $this->createOSFMedia($base,$poi['immagine'],$poi,004);
+            
+            $this->tags['image_gallery'][] = $this->createOSFMedia($poi['immagine'],$poi,004);
         }
         
         // Processing the gallery image of POI
         if (isset($poi['foto05']) && $poi['foto05']) {
             Log::info('Preparing OSF POI GALLERY foto05 with external POI ID: '.$this->source_id);
-            $base = 'https://sentieroitaliamappe.cai.it/index.php/view/media/getMedia?repository=sicaipubblico&project=SICAI_Pubblico&path=';
-            $this->tags['image_gallery'][] = $this->createOSFMedia($base,$poi['immagine'],$poi,005);
+            
+            $this->tags['image_gallery'][] = $this->createOSFMedia($poi['immagine'],$poi,005);
         }
 
         // Processing the poi_type
@@ -237,8 +232,8 @@ class OutSourceImporterFeatureSICAI extends OutSourceImporterFeatureAbstract {
      * @param array $media The OutSourceFeature parameters to be added or updated 
      * 
      */
-    public function createOSFMedia($base,$image,$poi,$suffix){ 
-
+    public function createOSFMedia($image,$poi,$suffix){ 
+        $base = 'https://sentieroitaliamappe.cai.it/index.php/view/media/getMedia?repository=sicaipubblico&project=SICAI_Pubblico&path=';
         Log::info('Preparing OSF MEDIA TRANSLATIONS with external ID: '.$poi['id_0']);
         $tags['name']['it'] = $poi['name'];
         try{
