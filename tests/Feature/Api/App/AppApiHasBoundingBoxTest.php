f5d37ae1b1c454c6ee45b2bd3b008331159c87c3
bongiu <peppedeka@gmail.com>
2022-07-20 18:26:21 +0200

    force dynamic conf when running tests

--- tests/Feature/Api/App/AppApiHasBoundingBoxTest.php
+++ tests/Feature/Api/App/AppApiHasBoundingBoxTest.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace Tests\Feature;
+
 use App\Models\App;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class AppApiHasBoundingBoxTest extends TestCase
@@ -18,11 +20,12 @@ class AppApiHasBoundingBoxTest extends TestCase
      */
     public function when_bbox_exist_then_config_api_has_bbox()
     {
-        $bbox_test = json_encode([1,2,3,4]);
-        $app = App::factory()->create(['api' => 'webapp','map_bbox'=>$bbox_test]);
+        $bbox_test = json_encode([1, 2, 3, 4]);
+        $app = App::factory()->create(['api' => 'webapp', 'map_bbox' => $bbox_test]);
+        Storage::disk('conf')->delete($app->id . '.json');
         $config_response = $this->getJson('/api/app/webapp/' . $app->id . '/config', []);
         $config_json = json_decode($config_response->getContent(), true);
-        
+
         $this->assertTrue(isset($config_json['MAP']));
         $this->assertTrue(isset($config_json['MAP']['bbox']));
 
