23c66fd61433f12dbc42823cade733355e1e3a3d
bongiu <peppedeka@gmail.com>
2022-10-05 12:28:26 +0200

    add_custom_icons_to_poi_types

--- app/Providers/WebmappAppIconProvider.php
+++ app/Providers/WebmappAppIconProvider.php
@@ -5,10 +5,51 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Bernhardh\NovaIconSelect\IconProvider;
+use App\Models\App;
 
-class WebmappAppIconProvider extends IconProvider {
-    public function __construct() {
+class WebmappAppIconProvider extends IconProvider
+{
+    public function __construct()
+    {
+
         $json = file_get_contents('css/icons/webmapp-icons/selection.json');
+        $allIconmoonSelection = App::all()->filter(function ($i, $k) {
+            return $i['iconmoon_selection'] != null;
+        })->map(function ($item, $key) {
+            $appInstance = str_replace('it.webmapp.', '', $item['app_id']);
+            $currentJson = json_decode($item['iconmoon_selection'], true);
+            $height = ($item['height']) ? $item['height'] : 1024;
+            $prevSize = ($item['prevSize']) ? $item['prevSize'] : 32;
+            if ($currentJson && $currentJson['icons'] && $currentJson['icons']) {
+                return collect($currentJson['icons'])->map(function ($item, $key) use ($appInstance, $height, $prevSize) {
+                    $height2 = $height / 2;
+
+                    $icon = $item['icon'];
+                    $svg = '';
+                    if (!is_null($icon["paths"])) {
+                        $svg = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 {$height} {$height}' width='{$prevSize}' height='{$prevSize}'><circle fill=\"darkorange\"  cx='{$height2}' cy='{$height2}' r='{$height2}'/><g fill=\"white\" transform='scale(0.8 0.8) translate(100, 100)'>";
+                        foreach ($icon["paths"] as $path) {
+                            $svg .=  "<path d='{$path}'/>";
+                        }
+                        $svg .= "</g></svg>";
+                    }
+                    $identifier = 'no-name';
+                    if (isset($icon['properties']) && isset($icon['properties']['name'])) {
+                        $identifier = $icon['properties']['name'];
+                    }
+                    if (isset($icon['tags']) && isset($icon['tags'][0])) {
+                        $identifier = $icon['tags'][0];
+                    }
+
+                    return [
+                        'label' =>  $identifier,
+                        'value' => $svg,
+                        'search' => [$identifier]
+                    ];
+                });
+            }
+            return null;
+        })->toArray();
         $content = json_decode($json, true);
         $options = array();
         foreach ($content['icons'] as $icon) {
@@ -19,6 +60,6 @@ class WebmappAppIconProvider extends IconProvider {
             ];
         }
 
-        $this->setOptions($options);
+        $this->setOptions(array_merge(...$allIconmoonSelection));
     }
 }
