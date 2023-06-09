5c279fd289fd016a0874d5b5e2e77e9eca04aca1
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-06-09 16:08:21 +0200

    Update Poi from Osm command

--- app/Console/Commands/UpdatePOIFromOsm.php
+++ app/Console/Commands/UpdatePOIFromOsm.php
@@ -11,15 +11,15 @@ class UpdatePOIFromOsm extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'geohub:update_pois_from_osm
+                            {user_email : the mail of the user of which the POIs must be updated}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
-
+    protected $description =  'Loops through all the pois belonging to the user identified by user_email. If the parameter osmid is not null, it performs some sync operations from OSM to GEOHUB.';
     /**
      * Create a new command instance.
      *
@@ -37,6 +37,82 @@ class UpdatePOIFromOsm extends Command
      */
     public function handle()
     {
-        return 0;
+        $userEmail = $this->argument('user_email');
+
+        if ($userEmail == null) {
+            $this->error('Please provide a user email');
+            return 0;
+        }
+
+        // Find the user based on the provided email
+        $user = User::where('email', $userEmail)->first();
+
+        if (!$user) {
+            $this->error('User not found');
+            return 0;
+        }
+
+        // Retrieve all pois belonging to the user
+        $pois = EcPoi::where('user_id', $user->id)->get();
+
+        $this->info('Updating pois for user ' . $user->name . ' (' . $user->email . ')...');
+
+        foreach ($pois as $poi) {
+            // Update the data for each poi
+            $this->updatePoiData($poi);
+        }
+
+        $this->info('Pois for user ' . $user->name . ' (' . $user->email . ') updated.');
+    }
+
+    // Update the data for a single poi
+    private function updatePoiData(EcPoi $poi)
+    {
+        if ($poi->osmid == null) {
+            return;
+        }
+
+        $this->info('Updating poi ' . $poi->name . ' (' . $poi->osmid . ')...');
+
+        try {
+            // Retrieve the geojson data from OSM based on the poi's osmid
+            $osmPoi = json_decode(OsmClient::getGeojson('node/' . $poi->osmid), true);
+        } catch (Exception $e) {
+            $this->error('Error while retrieving data from OSM for poi ' . $poi->name . ' (' . $poi->osmid . '). Error: ' . $e->getMessage());
+            return;
+        }
+
+        // Update the 'ele' attribute of the poi if it exists in the OSM data
+        $this->updatePoiAttribute($poi, $osmPoi, 'ele', 'ele');
+        // Update the 'ref' attribute of the poi if it exists in the OSM data
+        $this->updatePoiAttribute($poi, $osmPoi, 'ref', 'REF');
+        // Update the name of the poi if the 'name' key exists in the OSM data
+        $this->updatePoiName($poi, $osmPoi);
+
+        // Set the 'skip_geomixer_tech' field to true if the 'ele' attribute was updated
+        if ($poi->isDirty('ele')) {
+            $poi->skip_geomixer_tech = true;
+        }
+
+        // Save the updated poi
+        $poi->save();
+
+        $this->info('Poi ' . $poi->name . ' (osmid: ' . $poi->osmid . ') updated.');
+    }
+
+    // Update attribute of the poi if it exists in the OSM data
+    private function updatePoiAttribute(EcPoi $poi, array $osmPoi, string $poiAttributeKey, string $osmPropertyKey)
+    {
+        if (array_key_exists($osmPropertyKey, $osmPoi['properties']) && $osmPoi['properties'][$osmPropertyKey] != null) {
+            $poi->$poiAttributeKey = $osmPoi['properties'][$osmPropertyKey];
+        }
+    }
+
+    // Update the name of the poi if the 'name' key exists in the OSM data
+    private function updatePoiName(EcPoi $poi, array $osmPoi)
+    {
+        if (array_key_exists('name', $osmPoi['properties']) && $osmPoi['properties']['name'] != null) {
+            $poi->name = $osmPoi['properties']['REF'] . ' - ' . $osmPoi['properties']['name'];
+        }
     }
 }
