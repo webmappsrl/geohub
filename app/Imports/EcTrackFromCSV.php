4447d77d139b0a58c2bf4ba9d055908e6fe29878
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-09-25 18:26:09 +0200

    import class EcTrackFromCSV created

--- app/Imports/EcTrackFromCSV.php
+++ app/Imports/EcTrackFromCSV.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\EcTrack;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToModel;
 
 class EcTrackFromCSV implements ToModel
@@ -13,7 +14,36 @@ class EcTrackFromCSV implements ToModel
      * @return \Illuminate\Database\Eloquent\Model|null
      */
     public function model(array $row)
-    {
-        return new EcTrack([]);
+    { //ele_from = quota partenza
+        //ele_to = quota arrivo
+        //distance = km
+        //duration_forward = tempo percorrenza a-p
+        //duration_backward = tempo percorrenza p-a
+        //ele_max = quota massima 
+        //ele_min = quota minima
+        //ascent = dislivello totale UP
+        //descent = dislivello totale DOWN
+
+
+        foreach ($row as $key => $value) {
+            if ($row[0] == 'id')
+                continue;
+            Log::debug($row[1]);
+            Log::debug($row[3]);
+            EcTrack::updateOrCreate(['id' => $row[0]], [
+                'skip_geomixer_tech' => true,
+                'from' => $row[1],
+                'ele_from' => $row[2],
+                'to' => $row[3],
+                'ele_to' => $row[4],
+                'distance' => $row[6],
+                'duration_forward' => $row[7],
+                'duration_backward' => $row[8],
+                'ascent' => $row[9],
+                'descent' => $row[10],
+                'ele_min' => $row[11],
+                'ele_max' => $row[12],
+            ])->save();
+        }
     }
 }
