018ac91dca0d76208ab8522dd437648a133c5467
Pedram Katanchi <pedramkat@gmail.com>
2023-10-13 11:59:31 +0200

    added motomappa pois csv

--- app/Classes/OutSourceImporter/OutSourceImporterListStorageCSV.php
+++ app/Classes/OutSourceImporter/OutSourceImporterListStorageCSV.php
@@ -1,35 +1,39 @@
 <?php
+
 namespace App\Classes\OutSourceImporter;
 
 use Illuminate\Support\Facades\Storage;
 use App\Traits\ImporterAndSyncTrait;
 
-class OutSourceImporterListStorageCSV extends OutSourceImporterListAbstract { 
+class OutSourceImporterListStorageCSV extends OutSourceImporterListAbstract
+{
     use ImporterAndSyncTrait;
 
-    public function getTrackList():array {
+    public function getTrackList(): array
+    {
         $file = $this->CreateStoragePathFromEndpoint($this->endpoint);
         print_r($file);
         return [];
     }
 
-    public function getPoiList():array{
+    public function getPoiList(): array
+    {
         $path = $this->CreateStoragePathFromEndpoint($this->endpoint);
         $file = fopen($path, "r");
         $all_data = array();
         fgetcsv($file);
-        while ( ($row = fgetcsv($file, 1000, ",")) !==FALSE )
-           {
+        while (($row = fgetcsv($file, 1000, ",")) !== false) {
             $id = $row[0];
             $last_update = $row[1];
             $all_data[$id] = $last_update;
-           }
+        }
 
-         fclose($file);
+        fclose($file);
         return $all_data;
     }
 
-    public function getMediaList():array{
+    public function getMediaList(): array
+    {
         return [];
     }
 }
