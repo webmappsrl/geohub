3d1e663998a55ba9ef07efb2b4cccbaa57dc7b95
Pedram Katanchi <pedramkat@gmail.com>
2023-10-26 22:27:47 +0200

    ...

--- app/Observers/EcTrackElasticObserver.php
+++ app/Observers/EcTrackElasticObserver.php
@@ -88,12 +88,12 @@ class EcTrackElasticObserver
             foreach ($ecTrackLayers as $app_id => $layer_ids) {
                 if (!empty($layer_ids)) {
                     $ecTrack->elasticIndexUpsert('app_' . $app_id, $layer_ids);
-                    $ecTrack->elasticIndexUpsertLow('app_low_' . $app_id, $layer_ids);
-                    $ecTrack->elasticIndexUpsertHigh('app_high_' . $app_id, $layer_ids);
+                    // $ecTrack->elasticIndexUpsertLow('app_low_' . $app_id, $layer_ids);
+                    // $ecTrack->elasticIndexUpsertHigh('app_high_' . $app_id, $layer_ids);
                 } else {
                     $ecTrack->elasticIndexDelete('app_' . $app_id);
-                    $ecTrack->elasticIndexDelete('app_low_' . $app_id);
-                    $ecTrack->elasticIndexDelete('app_high_' . $app_id);
+                    // $ecTrack->elasticIndexDelete('app_low_' . $app_id);
+                    // $ecTrack->elasticIndexDelete('app_high_' . $app_id);
                 }
             }
         }
