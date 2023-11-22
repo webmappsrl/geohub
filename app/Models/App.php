2dbca86890c1f9148404e7cae321fabb255eb696
Pedram Katanchi <pedramkat@gmail.com>
2023-11-22 20:17:27 +0100

    added custom filter labels

--- app/Models/App.php
+++ app/Models/App.php
@@ -32,7 +32,7 @@ class App extends Model
     use HasTranslationsFixed;
 
     protected $fillable = ['welcome'];
-    public array $translatable = ['welcome', 'tiles_label', 'overlays_label','data_label','pois_data_label','tracks_data_label','page_project','page_disclaimer','page_credits'];
+    public array $translatable = ['welcome', 'tiles_label', 'overlays_label','data_label','pois_data_label','tracks_data_label','page_project','page_disclaimer','page_credits','filter_activity_label', 'filter_theme_label','filter_poi_type_label','filter_track_duration_label','filter_track_distance_label'];
     protected $casts = ['keywords' => 'array'];
     /**
      * The accessors to append to the model's array form.
