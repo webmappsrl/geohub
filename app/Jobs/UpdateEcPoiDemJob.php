d77f6fbacdbe84734c003ab0b2ec99e06e92a141
Pedram Katanchi <pedramkat@gmail.com>
2024-01-17 13:20:46 +0100

    added fix links in description command

--- app/Jobs/UpdateEcPoiDemJob.php
+++ app/Jobs/UpdateEcPoiDemJob.php
@@ -10,6 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class UpdateEcPoiDemJob implements ShouldQueue
 {
@@ -37,13 +38,18 @@ class UpdateEcPoiDemJob implements ShouldQueue
      */
     public function handle()
     {
-        $geom = $this->ecPoi->geometry;
-        $point_geom = DB::select("SELECT ST_Transform('$geom'::geometry,4326) AS geom")[0]->geom;
-        $coordinates = DB::select("SELECT ST_X('$point_geom') as x,ST_Y('$point_geom') AS y")[0];
-
-        $response = Http::get(rtrim(config('services.dem.host'), '/') . rtrim(config('services.dem.ele_api'), '/') . "/$coordinates->x/$coordinates->y");
-
-        $this->ecPoi->ele = $response->json()['ele'];
-        $this->ecPoi->save();
+        try {
+            $geom = $this->ecPoi->geometry;
+            $point_geom = DB::select("SELECT ST_Transform('$geom'::geometry,4326) AS geom")[0]->geom;
+            $coordinates = DB::select("SELECT ST_X('$point_geom') as x,ST_Y('$point_geom') AS y")[0];
+    
+            $response = Http::get(rtrim(config('services.dem.host'), '/') . rtrim(config('services.dem.ele_api'), '/') . "/$coordinates->x/$coordinates->y");
+    
+            $this->ecPoi->ele = $response->json()['ele'];
+            $this->ecPoi->save();
+        } catch (\Exception $e) {
+            // Handle the exception
+            Log::error('Error in UpdateEcPoiDemJob: ' . $e->getMessage());
+        }
     }
 }
