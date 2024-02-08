09b7828900fa08aad2cff011b15718d55e3cdcdc
Pedram Katanchi <pedramkat@gmail.com>
2024-02-08 08:52:25 +0100

    added action to app nova

--- app/Nova/Actions/GenerateUgcMediaRankingAction.php
+++ app/Nova/Actions/GenerateUgcMediaRankingAction.php
@@ -6,12 +6,18 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Artisan;
 use Laravel\Nova\Actions\Action;
 use Laravel\Nova\Fields\ActionFields;
 
 class GenerateUgcMediaRankingAction extends Action
 {
     use InteractsWithQueue, Queueable;
+
+    public function name(): string
+    {
+        return __("Generate Classification");
+    }
 
     /**
      * Perform the action on the given models.
@@ -22,7 +28,19 @@ class GenerateUgcMediaRankingAction extends Action
      */
     public function handle(ActionFields $fields, Collection $models)
     {
-        //
+        $command = 'geohub:get_ranked_users_near_pois'; // Specify the command you want to run
+        foreach ($models as $model) {
+            $parameters = ['--app_id' => $model->id];
+            try {
+                Artisan::call($command, $parameters);
+                $output = Artisan::output();
+                return Action::message('The command has been executed successfully!');
+            } catch (\Exception $e) {
+                return Action::danger('An error occurred: ' . $e->getMessage());
+            }
+        }
+
+        return Action::message('job executed');
     }
 
     /**
