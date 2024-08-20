31d4d2bcf71ad7ee74886e2c7a5eb01d27656c89
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2024-08-20 14:35:26 +0200

    chore: Update TaxonomyWhere
    Refactored the code for better readability and consistency by adjusting spacing, adding braces, and organizing method structures.

--- app/Nova/TaxonomyWhere.php
+++ app/Nova/TaxonomyWhere.php
@@ -24,7 +24,8 @@ use Yna\NovaSwatches\Swatches;
 use Tsungsoft\ErrorMessage\ErrorMessage;
 use Webmapp\FeatureImagePopup\FeatureImagePopup;
 
-class TaxonomyWhere extends Resource {
+class TaxonomyWhere extends Resource
+{
     use TabsOnEdit;
     /**
      * The model the resource corresponds to.
@@ -47,7 +48,8 @@ class TaxonomyWhere extends Resource {
         'name'
     ];
 
-    public static function group() {
+    public static function group()
+    {
         return __('Taxonomies');
     }
 
@@ -58,23 +60,24 @@ class TaxonomyWhere extends Resource {
      *
      * @return array
      */
-    public function fields(Request $request): array {
+    public function fields(Request $request): array
+    {
 
-        if(NovaCurrentResourceActionHelper::isIndex($request)) {
+        if (NovaCurrentResourceActionHelper::isIndex($request)) {
             return $this->index();
         }
 
-        if(NovaCurrentResourceActionHelper::isDetail($request)) {
+        if (NovaCurrentResourceActionHelper::isDetail($request)) {
             return $this->detail();
         }
 
-        if(NovaCurrentResourceActionHelper::isForm($request)) {
+        if (NovaCurrentResourceActionHelper::isForm($request)) {
             return $this->form($request);
         }
-
     }
 
-    private function index() {
+    private function index()
+    {
         return [
 
             NovaTabTranslatable::make([
@@ -87,54 +90,59 @@ class TaxonomyWhere extends Resource {
         ];
     }
 
-    private function detail() {
-        return [(new Tabs("Taxnonomy Where Details: {$this->name} ($this->id)",
-        [
-            'Main' => [
-                Text::make('Geohub ID',function(){return $this->id;}),
-                Text::make(__('Identifier'), 'identifier'),
-                BelongsTo::make('Author', 'author', User::class),
-                DateTime::make(__('Created At'), 'created_at'),
-                DateTime::make(__('Updated At'), 'updated_at'),        
-                NovaTabTranslatable::make([
-                    Text::make(__('Name'), 'name'),
-                    CKEditor::make(__('Description'), 'description'),
-                    Textarea::make(__('Excerpt'), 'excerpt'),
-                ]),     
-            ],
-            'Media' => [
-                ExternalImage::make(__('Feature Image'), function () {
-                    $url = isset($this->model()->featureImage) ? $this->model()->featureImage->url : '';
-                    if ('' !== $url && substr($url, 0, 4) !== 'http') {
-                        $url = Storage::disk('public')->url($url);
-                    }
-    
-                    return $url;
-                })->withMeta(['width' => 200]),    
-            ],
-            'Map' => [
-                WmEmbedmapsField::make(__('Map'), function ($model) {
-                    return [
-                        'feature' => $model->getGeojson(),
-                    ];
-                }),    
-            ],
-            'Style' => [
-                Number::make(__('Stroke Width'), 'stroke_width'),
-                Number::make(__('Stroke Opacity'), 'stroke_opacity'),
-                Text::make(__('Line Dash'), 'line_dash')->help('IMPORTANT : Write numbers with " , " separator'),
-                Number::make(__('Min Visible Zoom'), 'min_visible_zoom'),
-                Number::make(__('Max Size Zoom'), 'min_size_zoom'),
-                Number::make(__('Min Size'), 'min_size'),
-                Number::make(__('Max Size'), 'max_size'),
-                Number::make(__('Icon Zoom'), 'icon_zoom'),
-                Number::make(__('Icon Size'), 'icon_size'),
+    private function detail()
+    {
+        return [(new Tabs(
+            "Taxnonomy Where Details: {$this->name} ($this->id)",
+            [
+                'Main' => [
+                    Text::make('Geohub ID', function () {
+                        return $this->id;
+                    }),
+                    Text::make(__('Identifier'), 'identifier'),
+                    BelongsTo::make('Author', 'author', User::class),
+                    DateTime::make(__('Created At'), 'created_at'),
+                    DateTime::make(__('Updated At'), 'updated_at'),
+                    NovaTabTranslatable::make([
+                        Text::make(__('Name'), 'name'),
+                        CKEditor::make(__('Description'), 'description'),
+                        Textarea::make(__('Excerpt'), 'excerpt'),
+                    ]),
+                ],
+                'Media' => [
+                    ExternalImage::make(__('Feature Image'), function () {
+                        $url = isset($this->model()->featureImage) ? $this->model()->featureImage->url : '';
+                        if ('' !== $url && substr($url, 0, 4) !== 'http') {
+                            $url = Storage::disk('public')->url($url);
+                        }
+
+                        return $url;
+                    })->withMeta(['width' => 200]),
+                ],
+                'Map' => [
+                    WmEmbedmapsField::make(__('Map'), function ($model) {
+                        return [
+                            'feature' => $model->getGeojson(),
+                        ];
+                    }),
+                ],
+                'Style' => [
+                    Number::make(__('Stroke Width'), 'stroke_width'),
+                    Number::make(__('Stroke Opacity'), 'stroke_opacity'),
+                    Text::make(__('Line Dash'), 'line_dash')->help('IMPORTANT : Write numbers with " , " separator'),
+                    Number::make(__('Min Visible Zoom'), 'min_visible_zoom'),
+                    Number::make(__('Max Size Zoom'), 'min_size_zoom'),
+                    Number::make(__('Min Size'), 'min_size'),
+                    Number::make(__('Max Size'), 'max_size'),
+                    Number::make(__('Icon Zoom'), 'icon_zoom'),
+                    Number::make(__('Icon Size'), 'icon_size'),
+                ]
             ]
-        ]
         ))->withToolbar()];
     }
 
-    private function form($request) {
+    private function form($request)
+    {
 
         try {
             $geojson = $this->model()->getGeojson();
@@ -143,50 +151,93 @@ class TaxonomyWhere extends Resource {
         }
 
         $tab_title = "New Taxonomy Where";
-        if(NovaCurrentResourceActionHelper::isUpdate($request)) {
+        if (NovaCurrentResourceActionHelper::isUpdate($request)) {
             $tab_title = "Taxonomy Where Edit: {$this->name} ({$this->id})";
         }
 
-        return [(new Tabs($tab_title,
-        [
-            'Main' => [
-                Text::make(__('Identifier'), 'identifier'),
-                NovaTabTranslatable::make([
-                    Text::make(__('Name'), 'name'),
-                    CKEditor::make(__('Description'), 'description'),
-                    Textarea::make(__('Excerpt'), 'excerpt'),
-                ]),     
-            ],
-            'Media' => [
-                BelongsTo::make('Feature Image','featureImage',EcMedia::class)
-                ->searchable()
-                ->showCreateRelationButton()
-                ->nullable(),
-            ],
-            'Map' => [
-                File::make('Geojson')->store(function (Request $request, $model) {
-                    $content = file_get_contents($request->geojson);
-                    $geometry = $model->fileToGeometry($content);
+        return [(new Tabs(
+            $tab_title,
+            [
+                'Main' => [
+                    Text::make(__('Identifier'), 'identifier')
+                        ->help(__('API Identifier. To change the name displayed in the app, modify the label below.')),
+                    NovaTabTranslatable::make([
+                        Text::make(__('Name'), 'name')
+                            ->help(__('Name displayed in the overlay layers filter menu.')),
+                        CKEditor::make(__('Description'), 'description')
+                            ->help(__('Enter a detailed description of the taxonomy.')),
+                        Textarea::make(__('Excerpt'), 'excerpt')
+                            ->help(__('Provide a brief summary or excerpt for the taxonomy. This should be a concise description.')),
+                    ]),
+                ],
+                'Media' => [
+                    BelongsTo::make('Feature Image', 'featureImage', EcMedia::class)
+                        ->searchable()
+                        ->showCreateRelationButton()
+                        ->nullable()
+                        ->help(__('Select an image from Ec Media or upload one by clicking on "Create".')),
+                ],
+                'Map' => [
+                    File::make('Geojson')->store(function (Request $request, $model) {
+                        $content = file_get_contents($request->geojson);
+                        $geometry = $model->fileToGeometry($content);
 
-                    return $geometry ? [
-                        'geometry' => $geometry,
-                    ] : function () {
-                        throw new Exception(__("The uploaded file is not valid"));
-                    };
-                }),
-            ],
-            'Style' => [
-                Number::make(__('Stroke Width'), 'stroke_width'),
-                Number::make(__('Stroke Opacity'), 'stroke_opacity'),
-                Text::make(__('Line Dash'), 'line_dash')->help('IMPORTANT : Write numbers with " , " separator'),
-                Number::make(__('Min Visible Zoom'), 'min_visible_zoom'),
-                Number::make(__('Max Size Zoom'), 'min_size_zoom'),
-                Number::make(__('Min Size'), 'min_size'),
-                Number::make(__('Max Size'), 'max_size'),
-                Number::make(__('Icon Zoom'), 'icon_zoom'),
-                Number::make(__('Icon Size'), 'icon_size'),
-             ]
-        ]
+                        return $geometry ? [
+                            'geometry' => $geometry,
+                        ] : function () {
+                            throw new Exception(__("The uploaded file is not valid"));
+                        };
+                    })
+                        ->help(__('Here you can upload a GeoJSON file of a polygon, which is a closed geometry that defines an area on a map.')),
+                ],
+                'Style' => [
+                    Number::make(__('Stroke Width'), 'stroke_width')
+                        ->hideFromIndex()
+                        ->hideFromDetail()
+                        ->hideWhenCreating()
+                        ->hideWhenUpdating(),
+                    Number::make(__('Stroke Opacity'), 'stroke_opacity')
+                        ->hideFromIndex()
+                        ->hideFromDetail()
+                        ->hideWhenCreating()
+                        ->hideWhenUpdating(),
+                    Text::make(__('Line Dash'), 'line_dash')
+                        ->hideFromIndex()
+                        ->hideFromDetail()
+                        ->hideWhenCreating()
+                        ->hideWhenUpdating(),
+                    Number::make(__('Min Visible Zoom'), 'min_visible_zoom')
+                        ->hideFromIndex()
+                        ->hideFromDetail()
+                        ->hideWhenCreating()
+                        ->hideWhenUpdating(),
+                    Number::make(__('Max Size Zoom'), 'min_size_zoom')
+                        ->hideFromIndex()
+                        ->hideFromDetail()
+                        ->hideWhenCreating()
+                        ->hideWhenUpdating(),
+                    Number::make(__('Min Size'), 'min_size')
+                        ->hideFromIndex()
+                        ->hideFromDetail()
+                        ->hideWhenCreating()
+                        ->hideWhenUpdating(),
+                    Number::make(__('Max Size'), 'max_size')
+                        ->hideFromIndex()
+                        ->hideFromDetail()
+                        ->hideWhenCreating()
+                        ->hideWhenUpdating(),
+                    Number::make(__('Icon Zoom'), 'icon_zoom')
+                        ->hideFromIndex()
+                        ->hideFromDetail()
+                        ->hideWhenCreating()
+                        ->hideWhenUpdating(),
+                    Number::make(__('Icon Size'), 'icon_size')
+                        ->hideFromIndex()
+                        ->hideFromDetail()
+                        ->hideWhenCreating()
+                        ->hideWhenUpdating(),
+                ]
+            ]
         ))->withToolbar()];
     }
 
@@ -197,7 +248,8 @@ class TaxonomyWhere extends Resource {
      *
      * @return array
      */
-    public function cards(Request $request): array {
+    public function cards(Request $request): array
+    {
         return [];
     }
 
@@ -208,7 +260,8 @@ class TaxonomyWhere extends Resource {
      *
      * @return array
      */
-    public function filters(Request $request): array {
+    public function filters(Request $request): array
+    {
         return [];
     }
 
@@ -219,7 +272,8 @@ class TaxonomyWhere extends Resource {
      *
      * @return array
      */
-    public function lenses(Request $request): array {
+    public function lenses(Request $request): array
+    {
         return [];
     }
 
@@ -230,7 +284,8 @@ class TaxonomyWhere extends Resource {
      *
      * @return array
      */
-    public function actions(Request $request): array {
+    public function actions(Request $request): array
+    {
         return [];
     }
 }
