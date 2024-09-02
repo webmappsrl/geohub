8f0c846637a8b56dde342c839bfc1e8901bdadb7
bongiu <peppedeka@gmail.com>
2024-09-02 20:03:30 +0200

    chore: Update EcTrack Elastic Index Job
    - Added logging statements for debugging purposes
    - Moved the creation of UpdateEcTrackElasticIndexJob to always be executed, regardless of user_id condition

--- app/Jobs/UpdateEcTrackElasticIndexJob.php
+++ app/Jobs/UpdateEcTrackElasticIndexJob.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class UpdateEcTrackElasticIndexJob implements ShouldQueue
 {
@@ -34,12 +35,15 @@ class UpdateEcTrackElasticIndexJob implements ShouldQueue
      */
     public function handle()
     {
+        Log::info('UpdateEcTrackElasticIndexJob: ' . $this->ecTrack->id);
         $ecTrackLayers = $this->ecTrack->getLayersByApp();
+        Log::info('UpdateEcTrackElasticIndexJob-> ecTrackLayers: ' . $ecTrackLayers);
         $prefix = config('services.elastic.prefix') ?? 'geohub_app';
         if (!empty($ecTrackLayers)) {
             foreach ($ecTrackLayers as $app_id => $layer_ids) {
                 if (!empty($layer_ids)) {
                     $indexName = $prefix . '_' . $app_id;
+                    Log::info('UpdateEcTrackElasticIndexJob-> indexName: ' . $indexName);
                     $this->ecTrack->elasticIndex($indexName, $layer_ids);
                 } else {
                     //    DeleteEcTrackElasticIndexJob::dispatch($ecTrackLayers, $this->ecTrack->id);
