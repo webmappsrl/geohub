ac58a3d1340fae0f5cda0602efc7b05516d2b1dd
bongiu <peppedeka@gmail.com>
2024-09-23 14:26:54 +0200

    chore: Refactor PBFGenerator query for better readability and performance oc:4002
    - Updated the SQL query in the PBFGenerator class to improve readability and performance.
    - Reorganized the WHERE clause conditions for better logical flow.
    - Added a new line before a private method declaration for consistency.

--- app/Services/PBFGenerator.php
+++ app/Services/PBFGenerator.php
@@ -381,11 +381,11 @@ class PBFGenerator
                     ST_Union(geometry) AS geometry,
                     :strokeColor AS stroke_color
                 FROM ec_tracks
-                WHERE NOT ST_IsEmpty(geometry)
-                AND ST_Dimension(geometry) > 0
-                AND geometry IS NOT NULL
+   WHERE id IN ({$trackIdsStr})
+    AND NOT ST_IsEmpty(geometry)
+    AND ST_Dimension(geometry) > 0
+    AND geometry IS NOT NULL
             ";
-
 
                 // Esegui la query con i parametri
                 DB::statement($insertSql, [
@@ -399,6 +399,7 @@ class PBFGenerator
 
         Log::info('Fine populateTemporaryLayerTable');
     }
+
 
 
 
