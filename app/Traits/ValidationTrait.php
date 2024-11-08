e17157c5e0bba9ed67742d4860aa0769ad02c126
Alessandro <alessandropeci@webmapp.it>
2024-11-08 13:40:46 +0100

    chore: Update UgcPoiController and UgcTrackController
    - Added ValidationTrait to UgcPoiController and UgcTrackController
    - Refactored store method in UgcPoiController to support multiple versions
    - Added storeV1 and storeV2 methods in UgcPoiController for different versions
    - Removed validation logic from store method in UgcPoiController
    - Refactored store method in UgcTrackController to support multiple versions
    - Added storeV1 and storeV2 methods in UgcTrackController for different versions
    
    chore: Refactor UgcMediaController
    
    - Remove unused import statements
    - Move the ValidationTrait to be used in the UgcMediaController
    - Remove redundant code for populating properties and checking validation
    
    chore: Add ValidationTrait for handling data validation
    
    This commit adds a new file `ValidationTrait.php` under the `app/Traits` directory. The trait provides a method `checkValidation()` that takes in data and rules, and uses Laravel's Validator to validate the data against the given rules. If validation fails, it returns a response with the corresponding errors.
    
    chore: Update UgcTrackController store route to include optional version parameter
    
    The UgcTrackController store route in the api.php file has been updated to include an optional "version" parameter. This allows for more flexibility when storing track data.

---
+++ app/Traits/ValidationTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Validator;
+
+trait ValidationTrait
+{
+    protected function checkValidation($data, $rules)
+    {
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            $currentErrors = json_decode($validator->errors(), true);
+            $errors = [];
+            foreach ($currentErrors as $key => $error) {
+                $errors[$key] = $error;
+            }
+
+            return response(['error' => $errors], 400);
+        }
+    }
+} 
