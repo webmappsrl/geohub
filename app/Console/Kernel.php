dcc849e879a2755b31fe6f09c6ba2e6adf9b0a9e
bongiu <peppedeka@gmail.com>
2024-11-11 09:25:29 +0100

    chore: Update schedule in Kernel.php oc: 4253
    - Added daily tasks for GEOHUB DB DUMP, Index BLUBELL, Index CAIPARMA, Index PARCO MAREMMA, and Index FIE
    - Added hourly task for Sardegna Sentieri
    - Added weekly tasks for Import and Sync OSM2CAI
    - Added daily task for Sync Itinera Romanica Plus (commented out)
    - Added daily task for EUMA sync

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,41 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+
+        // GEOHUB DB DUMP ogni giorno alle 6
+        // $schedule->command('geohub:dump_db')->dailyAt('6:00');
+
+        // Index BLUBELL ogni giorno alle 20:30
+        $schedule->command('geohub:index-tracks 48')->dailyAt('20:30');
+
+        // Index CAIPARMA
+        $schedule->exec('bash /root/scripts/cai_parma_osm_poi_updated_at.sh')->dailyAt('18:00');
+        $schedule->command('geohub:update_pois_from_osm caiparma@webmapp.it')->dailyAt('20:15');
+        $schedule->command('geohub:feature_to_gallery poi 20703')->dailyAt('20:55');
+        $schedule->command('geohub:update_track_from_osm caiparma@webmapp.it "carlopr54@gmail.com"')->dailyAt('21:15');
+
+        // Index PARCO MAREMMA
+        $schedule->command('geohub:index-tracks 18 --no-elastic')->dailyAt('21:30');
+
+        // Index FIE
+        $schedule->command('geohub:index-tracks 29 --no-elastic')->dailyAt('20:00');
+
+        // Sardegna Sentieri ogni ora
+        $schedule->exec('bash /root/scripts/sardegna_sentieri_import_sync_updated_at.sh')->dailyAt('6:00');
+
+        // Import and Sync OSM2CAI
+        $schedule->exec('bash /root/geohub.webmapp.it/scripts/import_sync_osm2cai_all.sh')->saturdays()->at('22:00');
+        $schedule->exec('bash /root/scripts/osm2cai_hoqu_script.sh')->sundays()->at('1:00');
+        $schedule->command('geohub:index-tracks 15')->sundays()->at('2:00');
+        $schedule->command('geohub:index-tracks 26')->sundays()->at('4:00');
+        $schedule->command('geohub:generate_dem 26 dem')->sundays()->at('5:00');
+
+        // Sync Itinera Romanica Plus (se serve, rimuovi il commento)
+        // $schedule->exec('bash /root/scripts/ir_import_sync_hoqu.sh')->dailyAt('23:00');
+
+        $schedule->exec('bash /root/scripts/euma_sync_updated_at.sh')->dailyAt('3:00');
     }
+
 
     /**
      * Register the commands for the application.
@@ -34,7 +67,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
