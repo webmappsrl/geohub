ab8355676f6691b71b7a09964eec76c87364d16d
bongiu <peppedeka@gmail.com>
2024-11-11 10:21:51 +0100

    chore: Update subproject oc: 4174
    - Updated the `App.php` file in the `Nova` directory
    - Added a new file `wyswyg.php` to the `Nova/Fields` directory
    - Updated the `composer.json` file to include the package "alfonsobries/nova-trumbowyg"
    - Updated the `composer.lock` file with the new package version

---
+++ app/Nova/Fields/wyswyg.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Nova\Fields;
+
+use Alfonsobries\NovaTrumbowyg\NovaTrumbowyg;
+
+class NovaWyswyg extends NovaTrumbowyg
+{
+    public function __construct($name, $attribute = null, $resolveCallback = null)
+    {
+        parent::__construct($name, $attribute, $resolveCallback);
+
+        // Imposta le opzioni di default
+        $this->options([
+            'autogrow' => true,
+            'imageWidthModalEdit' => true,
+            'urlProtocol' => true,
+            'btns' => [
+                ['viewHTML'],
+                ['undo', 'redo'],
+                ['formatting'],
+                ['strong', 'em', 'del'],
+                ['superscript', 'subscript'],
+                ['link'],
+                ['insertImage'],
+                ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+                ['unorderedList', 'orderedList'],
+                ['horizontalRule'],
+                ['removeformat'],
+                ['fullscreen']
+            ]
+        ]);
+    }
+}
