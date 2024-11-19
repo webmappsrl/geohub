f7936c1e2681f8a2058cd8649b651a46a417b453
Marco Baroncini <100847596+mbaroncini@users.noreply.github.com>
2024-11-19 13:40:14 +0100

    fix: xdebug version to add compatibility with php 7.4

--- docker/configs/phpfpm/init-xdebug.sh
+++ docker/configs/phpfpm/init-xdebug.sh
@@ -2,6 +2,6 @@
 
 source .env
 containerName="php81_$APP_NAME"
-docker exec -it -u 0 $containerName pecl install xdebug
+docker exec -it -u 0 $containerName pecl install xdebug-3.1.6
 
 docker cp ./docker/configs/phpfpm/xdebug.ini $containerName:/usr/local/etc/php/conf.d/.
