8da3e759b5f5e98aa97b30b560a7384c82f6b327
marco <m.baroncini@cyberway.cloud>
2024-11-26 11:43:43 +0100

    fix: docker nodejs and xdebug minor error oc:4299

--- docker/configs/phpfpm/init-xdebug.sh
+++ docker/configs/phpfpm/init-xdebug.sh
@@ -2,6 +2,6 @@
 
 source .env
 containerName="php_$APP_NAME"
-docker exec -it -u 0 $containerName pecl install xdebug-3.1.6
+docker exec -it -u 0 $containerName pecl install xdebug-3.1.6 && touch /var/log/xdebug.log && chown www-data /var/log/xdebug.log
 
 docker cp ./docker/configs/phpfpm/xdebug.ini $containerName:/usr/local/etc/php/conf.d/.
