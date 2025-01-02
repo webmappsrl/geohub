62b4f5c95c490e59725375fdee9815d160946618
marco <m.baroncini@cyberway.cloud>
2025-01-02 09:13:24 +0100

    fix: update filesystems due a dev error oc: 4362

--- config/filesystems.php
+++ config/filesystems.php
@@ -90,7 +90,7 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_DUMPS_BUCKET'),
             'url' => env('AWS_DUMPS_URL', env('AWS_URL')),
-            'endpoint' => env('AWS_ENDPOINT'),
+            'endpoint' => env('AWS_DUMPS_ENDPOINT', env('AWS_ENDPOINT')),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false)
         ],
         'wmfeconf' => [
