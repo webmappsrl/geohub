d525df7dd3d6bdde057fee792c3c1e1367f7f76d
Marco Baroncini <100847596+mbaroncini@users.noreply.github.com>
2025-03-12 16:23:18 +0100

    fix: Update euma_sync_updated_at.sh

--- scripts/euma_sync_updated_at.sh
+++ scripts/euma_sync_updated_at.sh
@@ -7,9 +7,9 @@ php artisan geohub:out_source_importer_updated_at poi https://database.european-
 
 php artisan geohub:out_source_importer_updated_at track https://database.european-mountaineers.eu/api/v1/trail/list euma
 
-php artisan geohub:sync-ec-from-out-source-updated-at poi euma@webmapp.it --endpoint="https://database.european-mountaineers.eu/api/v1/hut/list" --provider=OutSourceImporterFeatureEUMA --name_format="{name}" --theme="euma-poi-huts"
+php artisan geohub:sync-ec-from-out-source-updated-at poi euma@webmapp.it --endpoint="https://database.european-mountaineers.eu/api/v1/hut/list" --provider=OutSourceImporterFeatureEUMA --name_format="{name}" --theme="euma-pois-huts"
 
-php artisan geohub:sync-ec-from-out-source-updated-at poi euma@webmapp.it --endpoint="https://database.european-mountaineers.eu/api/v1/climbingrockarea/list" --provider=OutSourceImporterFeatureEUMA --name_format="{name}" --theme="euma-poi-crags"
+php artisan geohub:sync-ec-from-out-source-updated-at poi euma@webmapp.it --endpoint="https://database.european-mountaineers.eu/api/v1/climbingrockarea/list" --provider=OutSourceImporterFeatureEUMA --name_format="{name}" --theme="euma-pois-crags"
 
 php artisan geohub:sync-ec-from-out-source-updated-at track euma@webmapp.it --endpoint="https://database.european-mountaineers.eu/api/v1/trail/list" --provider=OutSourceImporterFeatureEUMA --name_format="{name}" --activity="hiking" --theme=euma-trails
 
