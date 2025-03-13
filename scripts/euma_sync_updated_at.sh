776b7890146e2ab2338f074ef8c4a70c7b9dfb20
bongiu <peppedeka@gmail.com>
2025-03-13 16:14:11 +0100

    chore: Update euma_sync_updated_at.sh script
    - Removed unnecessary options from the geohub:sync-ec-from-out-source-updated-at command
    - Updated the geohub:index-tracks command to include a parameter for indexing tracks

--- scripts/euma_sync_updated_at.sh
+++ scripts/euma_sync_updated_at.sh
@@ -13,4 +13,4 @@ php artisan geohub:sync-ec-from-out-source-updated-at poi euma@webmapp.it --endp
 
 php artisan geohub:sync-ec-from-out-source-updated-at track euma@webmapp.it --endpoint="https://database.european-mountaineers.eu/api/v1/trail/list" --provider=OutSourceImporterFeatureEUMA --name_format="{name}" --activity="hiking" --theme=euma-trails
 
-php artisan geohub:index-tracks 30 --no-elastic
+php artisan geohub:index-tracks 30 
