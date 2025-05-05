5b42622c705f37db8ff545dacc6bd3d32e703053
bongiu <peppedeka@gmail.com>
2025-05-05 13:38:10 +0200

    fix(EcTrackObserver): remove useless condition

--- app/Observers/EcTrackElasticObserver.php
+++ app/Observers/EcTrackElasticObserver.php
@@ -43,26 +43,24 @@ class EcTrackElasticObserver
      */
     public function deleted(EcTrack $ecTrack)
     {
-        if ($ecTrack->user_id != 17482) { // TODO: Delete these 3 ifs after implementing osm2cai updated_ay sync
 
-            $ecTrackLayers = $ecTrack->getLayersByApp();
-            DeleteEcTrackElasticIndexJob::dispatch($ecTrackLayers, $ecTrack->id);
+        $ecTrackLayers = $ecTrack->getLayersByApp();
+        DeleteEcTrackElasticIndexJob::dispatch($ecTrackLayers, $ecTrack->id);
 
-            /**
-             * Delete track PBFs if the track has associated apps, a bounding box, and an author ID.
-             * Otherwise, log an info message.
-             *
-             * @param  EcTrack  $ecTrack  The track to observe.
-             * @return void
-             */
-            $apps = $ecTrack->trackHasApps();
-            $author_id = $ecTrack->user->id;
-            $bbox = $ecTrack->bbox($ecTrack->geometry);
-            if ($apps && $bbox && $author_id) {
-                DeleteTrackPBFJob::dispatch($apps, $author_id, $bbox);
-            } else {
-                Log::info('No apps or bbox or author_id found for track '.$ecTrack->id.' to delete PBFs.');
-            }
+        /**
+         * Delete track PBFs if the track has associated apps, a bounding box, and an author ID.
+         * Otherwise, log an info message.
+         *
+         * @param  EcTrack  $ecTrack  The track to observe.
+         * @return void
+         */
+        $apps = $ecTrack->trackHasApps();
+        $author_id = $ecTrack->user->id;
+        $bbox = $ecTrack->bbox($ecTrack->geometry);
+        if ($apps && $bbox && $author_id) {
+            DeleteTrackPBFJob::dispatch($apps, $author_id, $bbox);
+        } else {
+            Log::info('No apps or bbox or author_id found for track ' . $ecTrack->id . ' to delete PBFs.');
         }
     }
 
